3865f305f4b552b17b77c4eab03d71bbbba8e47a
MatheusRamosDeLima <matheusramosej@gmail.com>
2024-01-30 10:06:39 -0300

    Adição dos links das fontes especiais e uso de variáveis para as fontes e outros

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/layout.css">
+    <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@400;700&family=Poppins:wght@400;700&family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">
     @yield('css')
     <script src="/js/layout.js" defer></script>
     @yield('js')
