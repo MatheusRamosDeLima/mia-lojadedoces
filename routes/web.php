a9b702e96b98cc0844434bc84b6d98155f147d3f
MatheusRamosDeLima <matheusramosej@gmail.com>
2024-01-29 22:01:45 -0300

    Complemento do commit anterior '-'

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\HomeController;
 Route::get('/', [HomeController::class, 'index']);
 
 use App\Http\Controllers\SobreController;
-Route::get('/', [SobreController::class, 'index']);
+Route::get('/sobre', [SobreController::class, 'index']);
 
 use App\Http\Controllers\ContatoController;
-Route::get('/', [ContatoController::class, 'index']);
+Route::get('/contato', [ContatoController::class, 'index']);
